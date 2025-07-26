29ec198dcaae2e69e35843aaed9c9f8ad5dbc30c
Salavat Akhiyarov <salavatahiyarov@icloud.com>
2025-07-27 00:04:18 +0300

    7

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kittygram99.duckdns.org', 'localhost', '51.250.17.35', '127.0.0.1']
 
